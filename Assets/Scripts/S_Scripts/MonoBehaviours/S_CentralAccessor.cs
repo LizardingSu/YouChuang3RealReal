0285d865a6a3b928515e0f2da395959e672d7761
LizardingSu <2510819365@qq.com>
2023-03-26 15:15:13 +0800

    1

---
+++ Assets/Scripts/S_Scripts/MonoBehaviours/S_CentralAccessor.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_CentralAccessor : MonoBehaviour
+{
+    [Header("Managers")]
+    public S_StateManager StateManager;
+    public S_AudioManager AudioManager;
+    public S_ProcessManager ProcessManager;
+}
